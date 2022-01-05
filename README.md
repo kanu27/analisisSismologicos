c151d6213557d6a5c673c72c22b47ffb41415355
kanu27 <kanu.ponce@gmail.com>
2022-01-05 10:05:18 -0300

    avance en manipulacion de datos

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 pip3 install -r config/requirements.txt
 sudo apt-get install python3-tk
+
+
+
+
+
+
+
+
+api geolocalizacion : https://developers.google.com/maps/documentation/geocoding/get-api-key
--- src/main.py
+++ src/main.py
@@ -1,22 +1,62 @@
 import pandas as pd
 import numpy as np
 import matplotlib
+import os
+import http.client
+
 
 
 def getData():
     df = pd.read_csv(
         'src/data/sismosMundiales.csv',
-        sep=","
+        sep=",",
         )
-    df = df.fillna(0)
-    dfCovid = pd.DataFrame(data=df)
-    return dfCovid
+    return df.head()
+
+def GetCountry(df):
+    conn = http.client.HTTPSConnection("maps.googleapis.com")
+    payload = ''
+    headers = {}
+    conn.request("GET", "/maps/api/geocode/json?latlng=43.609,12.336&key=AIzaSyCQrCqobvEVkWXFdyUUsKXo4KHg-520Lr8", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+
+    lati = df['Lat']
+    long = df['Lon']
+    df.replace({"0-17": 17, "+55": 60})
+
+def modifyData():
+    dfDate = getData()
+    dfDate['GeneralTime'] = pd.to_datetime(dfDate['Timestamp'], unit='s')
+    dfDate.set_index('Timestamp', inplace = True)
+    name = dfDate["Region"].str.split(',',expand=True)
+    name.columns = ['Location', 'Country']
+    dfDate = pd.concat([dfDate, name], axis=1)
+    dfDate.drop(['Year', 'Month', 'Day', 'Time', 'Region'], axis = 'columns', inplace=True)
+    dfDate.replace(to_replace=[None], value=np.nan, inplace=True)
+    
+    #print()
+    autopct=lambda country: GetCountry(dfDate[dfDate['Country'].isnull()])
+ 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+    
 if __name__ == "__main__":
-    getData()
+    modifyData()
 
 
 
